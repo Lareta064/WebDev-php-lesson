0069d5984b99fac927014fd8fe23d4c95a6b94cf
Lareta <lareta064@gmail.com>
2018-09-16 14:50:36 +0300

    function greeting last version

--- index2.php
+++ index2.php
@@ -1,45 +1,22 @@
 <?php 
-
-	 function greeting($name, $dayNumber){
-
-	 		/*вариант через switch case*/
-	 	echo "<h3> Используем switch case </h3>";
-	 	echo "Привет $name, ";
-
-	 		switch($dayNumber){
-		 		case(1): case(2): case(3): case(4): case(5):
-		 		echo "хорошего рабочего дня!";
-		 		break;
-
-		 		case(6): case(7):
-		 		echo "хорошего выходного дня!";
-		 		break;
-		 		default:
-		 		echo "странный день недели, даже не знаю чего пожелать :(";
-	 	}	 	
-	}
-
-	greeting('Лара', 6);
-
-	    echo "<br><br>";
-
-	    /*вариант через if*/
-
-		 function greetingToo($name, $dayNumber){
-
-		 echo "<h3> Используем условие if </h3>";
+	function greeting($name, $dayNumber){		 
 
 	 	if( $dayNumber >=1 && $dayNumber <6){
-	 		echo "Привет $name, хорошего рабочего дня!";
-	 	} else if($dayNumber ==6 || $dayNumber ==7){
-	 		echo "Привет $name, хорошего выходного дня!";
-	 	}
-	 	else{
-	 		echo "Привет $name, странный день недели, даже не знаю чего пожелать :(";
-	 	}
-	}
 
-	greetingToo('Лара', 6);
+	 		echo "Привет $name!  Хорошего и продуктивного рабочего дня!";
+	 		
+		 	} else {
+		 		
+		 		switch($dayNumber){		 		
+			 		case(6): case(7):
+			 		echo " Привет $name! Желаю Вам хорошо отдохнуть в этот день!";
+			 		break;
+			 		default:
+			 		echo "Привет $name! Странный день недели, даже не знаю чего пожелать :(";
+		 		}	 		
+		 	}	 	
+		}
 
+	greeting('Лара',7);
 
  ?>
